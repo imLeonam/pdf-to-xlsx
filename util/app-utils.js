601755429d4311a76d1d855cd2e6b73cef11813c
imLeonam <leonammrib@gmail.com>
2024-02-05 21:09:40 -0300

    feat(util): organizing headers

--- util/app-utils.js
+++ util/app-utils.js
@@ -2,15 +2,27 @@ function removeAccents(text) {
   return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/ç/g, 'c');
 }
 
+function chunkArray(array, sizes) {
+  const chunkedArray = [];
+  let currentIndex = 0;
+
+  for (let size of sizes) {
+    chunkedArray.push(array.slice(currentIndex, currentIndex + size));
+    currentIndex += size;
+  }
+
+  return chunkedArray;
+}
+
 function mountHeaders(data) {
   data = data.text.split('\n');
   data.shift();
   data.shift();
 
-  const headers = data[1].concat(' ').concat(data[2]).concat(' ').concat(data[3]).concat(' ').concat(data[4]).split(' ');
+  let headers = data[1].concat(' ').concat(data[2]).concat(' ').concat(data[3]).concat(' ').concat(data[4]).split(' ');
 
   const regex = /^[a-z]/;
-  return headers.reduce((acumulator, header, i) => {
+  const formattedHeaders = headers.reduce((acumulator, header, i) => {
     header = removeAccents(header);
     const next = headers[i + 1];
 
@@ -30,6 +42,10 @@ function mountHeaders(data) {
     acumulator.push(header);
     return acumulator;
   }, []);
+
+  const newHeaders = chunkArray(formattedHeaders, [5, 4, 5, 2]);
+
+  return newHeaders;
 }
 
 
